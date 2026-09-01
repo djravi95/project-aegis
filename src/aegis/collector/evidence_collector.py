f008216bc7e6f65cd63c863ead1841a5cf52afc2
Dheeraj Ravi <djravi95@gmail.com>
2026-09-01 13:37:18 +0530

    Initialie the Evidence Collector Module

---
+++ src/aegis/collector/evidence_collector.py
@@ -0,0 +1,10 @@
+class EvidenceCollector:
+    """Collects all the evidences required for flood assessment"""
+
+    def __init__(self):
+        pass
+
+    def collect(self,district:str):
+        """Collects evidences for the selcted district"""
+        pass
+    
