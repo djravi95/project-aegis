7ef0c35c09fc068f16b3ec851ceb646842a114fc
Dheeraj Ravi <djravi95@gmail.com>
2026-09-02 16:35:03 +0530

    Completed the collector structuring

--- src/aegis/collector/evidence_collector.py
+++ src/aegis/collector/evidence_collector.py
@@ -1,10 +1,26 @@
+from .rainfall_collector import RainfallCollector
+from .evidence_package import EvidencePackage
+from datetime import datetime
+
+
 class EvidenceCollector:
-    """Collects all the evidences required for flood assessment"""
+    """Coordinates all evidence collectors for a flood-risk analysis"""
+
 
     def __init__(self):
-        pass
+        self.rainfall_collector = RainfallCollector()
 
-    def collect(self,district:str):
-        """Collects evidences for the selcted district"""
-        pass
-    
+    def collect(self, district:str)-> EvidencePackage:
+            rainfall = self.rainfall_collector.collect(district)
+
+            return EvidencePackage(
+
+                district=district,
+                analysis_time=datetime.now(),
+                evidences={"rainfall": rainfall},
+                collection_status="Completed",
+            )
+
+
+
+
--- src/aegis/main.py
+++ src/aegis/main.py
@@ -1,4 +1,9 @@
+from collector.evidence_collector import EvidenceCollector
+
 def main():
+    collector = EvidenceCollector()
+    package = collector.collect("Hyderabad")
+    print(package)
     print("Project Aegis is operational.")
 
 
